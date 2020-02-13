ae094816199b2c97280fa1bc8e90982f491e3a05
0x0D1n <kevinschmidt_113@hotmail.com>
2020-02-13 17:38:51 +0100

    Switching to CMD instead of web tool

---
+++ main.py
@@ -0,0 +1,12 @@
+from huaweiapi import *
+
+def main():
+    ### Command line -helper for args
+    #import argparse
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument()
+    print(getDeviceInformation("192.168.8.1"))
+
+
+if __name__ == "__main__":
+    main()
